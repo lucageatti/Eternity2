e5de6e99307707853c74bc251f374e277cfa8922
Luca Geatti <geatti.luca@spes.uniud.it>
2017-03-05 14:42:53 +0100

    Add tests to the relation

---
+++ e2test.sh
@@ -0,0 +1,26 @@
+
+#!/bin/bash
+
+MAX_TESTS=20
+
+echo "*** Multistart Steepest Descent ***"
+echo 
+echo "Instance $INST"
+echo "Method $METHOD"
+
+
+for i in `seq 1 $MAX_TESTS`;
+do
+    SEED="$(head -c 100 /dev/urandom | LC_CTYPE=C tr -dc '[:digit:]')"
+    if [ -z "$SEED" ];
+    then
+        SEED=0
+    fi
+
+    echo "Test $i) "
+    echo "Seed: $SEED"
+    SOL="$(./Eternity2 --main::instance "../eternity2-data/pieces_set_2/e2pieces.txt" --main::seed $SEED < ../eternity2-data/ourInput/input_e2.txt)"
+    echo $SOL | grep -o "CURRENT COST : [0-9]*" | tail -1
+    echo $SOL | grep -o "ELAPSED TIME : [0-9]*\.[0-9]*s" | tail -1 
+    echo
+done
